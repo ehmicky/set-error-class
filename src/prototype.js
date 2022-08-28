c32be4768a6261853b067bffb2bcf3563efd50c4
ehmicky <ehmicky@gmail.com>
2022-08-28 23:39:09 +0200

    Continue implementation

---
+++ src/prototype.js
@@ -0,0 +1,1 @@
+export const updatePrototype = function (error, ErrorClass) {}
--- src/stack.js
+++ src/stack.js
@@ -1,40 +1,74 @@
-// We try to find the current message in `error.stack` and replace it.
-// If we cannot find it, we insert it instead.
-//  - This is a common situation when `error.message` was modified without
-//    re-computing `error.stack`
-//  - This ensures the next call to `set-error-message` will now find
-//    `currentMessage`
-// `error.stack` is not standard so we do not try to assume its format.
-export const getStack = function ({ name, stack }, newMessage, currentMessage) {
-  return currentMessage !== '' && stack.includes(currentMessage)
-    ? replaceMessage({ name, stack, newMessage, currentMessage })
-    : insertMessage(name, stack, newMessage)
+// In some JavaScript engines, `error.stack` includes `error.name`, but is
+// not updated when `error.name` is modified. This fixes this.
+export const updateStack = function (error, currentName) {
+  if (!shouldUpdateStack(error, currentName)) {
+    return
+  }
+
+  const stack = getStack(error, currentName)
+  setNonEnumProp(error, 'stack', stack)
 }
 
+const shouldUpdateStack = function (error, currentName) {
+  return (
+    SHOULD_UPDATE_STACK &&
+    currentName !== error.name &&
+    currentName !== '' &&
+    error.stack.includes(currentName)
+  )
+}
+
+// Only V8 includes `error.name` in `error.stack`
+const stackIncludesName = function () {
+  // eslint-disable-next-line fp/no-class
+  class StackError extends Error {}
+  const descriptor = {
+    value: EXAMPLE_NAME,
+    enumerable: false,
+    writable: true,
+    configurable: true,
+  }
+  // eslint-disable-next-line fp/no-mutating-methods
+  Object.defineProperty(StackError, 'name', descriptor)
+  // eslint-disable-next-line fp/no-mutating-methods
+  Object.defineProperty(StackError.prototype, 'name', descriptor)
+  const { stack } = new StackError('')
+  return typeof stack === 'string' && stack.includes(EXAMPLE_NAME)
+}
+
+const EXAMPLE_NAME = 'SetErrorClassError'
+const SHOULD_UPDATE_STACK = stackIncludesName()
+
+const setNonEnumProp = function (error, propName, value) {
+  // eslint-disable-next-line fp/no-mutating-methods
+  Object.defineProperty(error, propName, {
+    value,
+    enumerable: false,
+    writable: true,
+    configurable: true,
+  })
+}
+
+// We try to find the current name in `error.stack` and replace it.
+// If we cannot find it, this is a noop.
+//  - Unlike `error.message`, it is not as important for `error.name` to be
+//    missing from the stack
 // We only replace the first instance to prevent modifying the stack lines
-// in case the message is also a file name present in those.
-const replaceMessage = function ({ name, stack, newMessage, currentMessage }) {
-  const replacers = getReplacers(name, newMessage, currentMessage)
+// in case the `name` is also a filename present in those.
+// `error.stack` is not standard so we do not try to assume its format.
+const getStack = function ({ name, stack }, currentName) {
+  const replacers = getReplacers(currentName, name)
   const [fromA, to] = replacers.find(([from]) => stack.includes(from))
   return stack.replace(fromA, to)
 }
 
 // We try a series of most to least specific patterns to prevent changing
-// messages present in previews (e.g. from Node.js `--enable-source-maps`).
-const getReplacers = function (name, newMessage, currentMessage) {
+// error names present in previews (e.g. from Node.js `--enable-source-maps`).
+const getReplacers = function (currentName, newName) {
   return [
-    [`${name}: ${currentMessage}`, `${name}: ${newMessage}`],
-    [`: ${currentMessage}`, `: ${newMessage}`],
-    [`\n${currentMessage}`, `\n${newMessage}`],
-    [` ${currentMessage}`, ` ${newMessage}`],
-    [currentMessage, newMessage],
+    [`\n${currentName}: `, `\n${newName}: `],
+    [`${currentName}: `, `${newName}: `],
+    [`${currentName} `, `${newName} `],
+    [currentName, newName],
   ]
-}
-
-const insertMessage = function (name, stack, newMessage) {
-  const errorName = `${name}: `
-  const newMessageA = `${newMessage.trimEnd()}\n`
-  return stack.startsWith(errorName)
-    ? stack.replace(errorName, `${errorName}${newMessageA}`)
-    : `${errorName}${newMessageA}${stack}`
 }
