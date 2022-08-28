dd16bd0cd173883ac796fd9656c60e1e99a426e5
ehmicky <ehmicky@gmail.com>
2022-08-28 23:53:18 +0200

    Add more tests

--- test/main.js
+++ test/main.js
@@ -1,9 +1,32 @@
 import test from 'ava'
 import setErrorClass from 'set-error-class'
+import { each } from 'test-each'
 
-test('Sets the class', (t) => {
-  const error = new Error('test')
-  t.false(error instanceof TypeError)
-  setErrorClass(error, TypeError)
-  t.true(error instanceof TypeError)
+// eslint-disable-next-line unicorn/no-null
+each([undefined, null, '', {}], ({ title }, notAnError) => {
+  test(`Normalizes the error | ${title}`, (t) => {
+    t.true(setErrorClass(notAnError, Error) instanceof Error)
+  })
+})
+
+test('Returns the error', (t) => {
+  const error = new Error('one')
+  t.is(setErrorClass(error, Error), error)
+})
+
+each(['one', undefined], ({ title }, stack) => {
+  test.serial(
+    `Only update error stack if engine includes name in it | ${title}`,
+    (t) => {
+      // eslint-disable-next-line fp/no-mutation
+      Error.prepareStackTrace = () => stack
+
+      const error = new Error('one')
+      setErrorClass(error, TypeError)
+      t.false(error.stack.includes('TypeError'))
+
+      // eslint-disable-next-line fp/no-delete
+      delete Error.prepareStackTrace
+    },
+  )
 })
---
+++ test/prototype.js
@@ -0,0 +1,9 @@
+import test from 'ava'
+import setErrorClass from 'set-error-class'
+
+test('Sets the prototype', (t) => {
+  const error = new Error('test')
+  t.false(error instanceof TypeError)
+  setErrorClass(error, TypeError)
+  t.true(error instanceof TypeError)
+})
