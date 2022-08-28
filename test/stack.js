01e163e258ba559fcf70277a7cfc0158cc653ae8
ehmicky <ehmicky@gmail.com>
2022-08-28 23:54:56 +0200

    Add more tests

--- test/stack.js
+++ test/stack.js
@@ -24,10 +24,29 @@ test('Updates stack', (t) => {
   error.stack = 'Error: one\nstackLines'
   setErrorClass(error, TypeError)
   t.is(error.stack, 'TypeError: one\nstackLines')
+  t.false(Object.getOwnPropertyDescriptor(error, 'stack').enumerable)
 })
 
-each(['', 'Other'], ({ title }, name) => {
-  test(`Is a noop if current name cannot be found | ${title}`, (t) => {
+each(
+  [
+    ['Error: one', 'TypeError: one'],
+    ['one Error: one\nError: one', 'one Error: one\nTypeError: one'],
+    ['Error Error: one', 'Error TypeError: one'],
+    ['ErrorOne Error one', 'ErrorOne TypeError one'],
+    ['ErrorOne', 'TypeErrorOne'],
+  ],
+  ({ title }, [from, to]) => {
+    test(`Prevent injecting in stack header | ${title}`, (t) => {
+      const error = new Error('one')
+      error.stack = `${from}\nstackLines`
+      setErrorClass(error, TypeError)
+      t.is(error.stack, `${to}\nstackLines`)
+    })
+  },
+)
+
+each(['', 'Other', 'TypeError'], ({ title }, name) => {
+  test(`Is a noop if current name cannot be found or has not changed | ${title}`, (t) => {
     const error = new Error('one')
     const { stack } = error
     t.is(setErrorClass(error, TypeError, name).stack, stack)
